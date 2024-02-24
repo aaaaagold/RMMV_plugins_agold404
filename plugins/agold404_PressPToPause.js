b5cb56dfeb8130195df6ef2aaf62277355466587
OAO <10041465+aaaaagold@users.noreply.github.com>
2024-02-25 01:27:20 +0800

    add PressPToPause

---
+++ plugins/agold404_PressPToPause.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+/*:
+ * @plugindesc press P to pause
+ * @author agold404
+ * @help .
+ * 
+ * This plugin can be renamed as you want.
+ */
+
+(()=>{ let k,r,t;
+
+const keyCode=80; // P
+
+new cfc(SceneManager).add('requestUpdate',function f(){
+	if(this._isPressPPaused) return;
+	return f.ori.apply(this,arguments);
+}).getP()._isPressPPaused=false;
+
+const f=window._pressPToPause=e=>{ if(e.keyCode!==keyCode) return;
+	if((SceneManager._isPressPPaused=!SceneManager._isPressPPaused)){
+		$gameSystem.saveBg();
+		AudioManager.stopAll();
+		return;
+	}
+	$gameSystem.replayBg(true);
+	SceneManager.resume();
+};
+document.addEventListener('keydown',f);
+
+})();
