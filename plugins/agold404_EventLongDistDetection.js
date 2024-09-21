e99f20cfbbece97a77ba26dcc0ae618f3c431c17
agold404 <10041465+aaaaagold@users.noreply.github.com>
2024-09-21 16:29:21 +0800

    add EventLongDistDetection

---
+++ plugins/agold404_EventLongDistDetection.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+/*:
+ * @plugindesc <longDistDetection> makes event movable from player locating in the map everywhere
+ * @author agold404
+ * @help <longDistDetection>
+ * 
+ * This plugin can be renamed as you want.
+ */
+
+(()=>{ let k,r,t;
+
+new cfc(Game_Event.prototype).add('isNearTheScreen',function f(){
+	return (f.tbl[0] in this.getMeta())||f.ori.apply(this,arguments);
+},t=[
+'longDistDetection',
+]).add('isNearThePlayer',function f(){
+	return (f.tbl[0] in this.getMeta())||f.ori.apply(this,arguments);
+},t);
+
+})();
