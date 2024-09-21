ed1dfa0bf69c4925828ba700c92706a326c4727c
agold404 <10041465+aaaaagold@users.noreply.github.com>
2024-09-21 12:57:56 +0800

    update ExpandEventRange: apply Game_Event.getMeta

--- plugins/agold404_ExpandEventRange.js
+++ plugins/agold404_ExpandEventRange.js
@@ -2,8 +2,9 @@
 /*:
  * @plugindesc expand event trigger / collide range
  * @author agold404
- * @help page note
+ * @help in note
  * <expandRange:positive_integer_size>
+ * e.g. <expandRange:2>
  * 
  * This plugin can be renamed as you want.
  */
@@ -11,7 +12,7 @@
 (()=>{ let k,r,t;
 
 new cfc(Game_Event.prototype).add('getPosKeys',function f(dx,dy){
-	const er=this.getExpandRange()|0; if(!(0<er)) return [this.getPosKey(dx,dy)];
+	const er=this.getExpandRangeA()|0; if(!(0<er)) return [this.getPosKey(dx,dy)];
 	const rtv=[];
 	dx|=0;
 	dy|=0;
@@ -21,19 +22,13 @@ new cfc(Game_Event.prototype).add('getPosKeys',function f(dx,dy){
 	return rtv;
 }).add('setupPageSettings',function f(){
 	const page=f.ori.apply(this,arguments);
-	const meta=page&&page.meta; if(!meta) return page;
-	this._expandRange=meta.expandRange|0;
+	const meta=this.getMeta();
+	this._expandRangeA=meta.expandRange|0;
 	return page;
-}).add('getExpandRange',function f(){
-	return this._expandRange;
-}).add('isNearTheScreen',function f(){
-	return (f.tbl[0] in this.event().meta)||f.ori.apply(this,arguments);
-},t=[
-'longDistDetection',
-]).add('isNearThePlayer',function f(){
-	return (f.tbl[0] in this.event().meta)||f.ori.apply(this,arguments);
-},t).add('_canPass_expandRange',function f(x,y,d,thePointOnly,oriFunc){
-	const er=this.getExpandRange(); if(thePointOnly||!(0<er)) return oriFunc.apply(this,arguments);
+}).add('getExpandRangeA',function f(){
+	return this._expandRangeA;
+},undefined,true,true).add('_canPass_expandRange',function f(x,y,d,thePointOnly,oriFunc){
+	const er=this.getExpandRangeA(); if(thePointOnly||!(0<er)) return oriFunc.apply(this,arguments);
 	const dx0=d===6?0:-er,dx1=d===4?0:er;
 	const dy0=d===2?0:-er,dy1=d===8?0:er;
 	for(let dy=dy0;dy<=dy1;++dy){ for(let dx=dx0;dx<=dx1;++dx){
@@ -68,7 +63,7 @@ else new cfc(p).add(k,function f(x,y,d,thePointOnly,reservedForPassingOriFunc){
 
 new cfc(Game_Map.prototype).add('update_locTbl_addEvt',function f(evt,coord){
 	if(!coord) return;
-	if(!(0<evt.getExpandRange())) return f.ori.apply(this,arguments);
+	if(!(0<evt.getExpandRangeA())) return f.ori.apply(this,arguments);
 	evt.getPosKeys().forEach(f.tbl[0],arguments);
 },[
 function f(key){
@@ -77,7 +72,7 @@ function f(key){
 	cont.uniquePush(evt);
 },
 ]).add('update_locTbl_delEvt',function f(evt,coord,x,y){
-	const er=evt.getExpandRange(); if(!(0<er)) return f.ori.apply(this,arguments);
+	const er=evt.getExpandRangeA(); if(!(0<er)) return f.ori.apply(this,arguments);
 	for(let dy=-er;dy<=er;++dy){ for(let dx=-er;dx<=er;++dx){
 		arguments[2]=x+dx;
 		arguments[3]=y+dy;
