55c5431566eb8c291917074bd7d2680257b957fe
agold404 <10041465+aaaaagold@users.noreply.github.com>
2025-02-13 01:35:00 +0800

    add DefaultOn_alwaysDash

---
+++ plugins/agold404_DefaultOn_alwaysDash.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+/*:
+ * @plugindesc Make default of "Always Dash" be on
+ * @author agold404
+ * 
+ * @help .
+ * 
+ * 
+ * This plugin can be renamed as you want.
+ */
+
+(()=>{ let k,r,t;
+const pluginName=getPluginNameViaSrc(document.currentScript.getAttribute('src'))||"agold404_DefaultOn_alwaysDash";
+const params=PluginManager.parameters(pluginName)||{};
+
+t=[
+undefined,
+params, // 1: plugin params
+];
+
+new cfc(ConfigManager).add('applyData',function f(config){
+	if(config.alwaysDash===undefined) config.alwaysDash=true;
+	return f.ori.apply(this,arguments);
+});
+
+
+})();
