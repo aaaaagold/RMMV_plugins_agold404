84dd32080b4501e0cd305c58b17874fdf5bf3f33
agold404 <10041465+aaaaagold@users.noreply.github.com>
2025-02-16 21:04:19 +0800

    update CopyEvent: multiple template by map id

--- plugins/agold404_CopyEvent.js
+++ plugins/agold404_CopyEvent.js
@@ -4,6 +4,7 @@
  * @author agold404
  * @help $gameMap.cpevt(evtId,x,y,direction=undefined);
  * $gameMap.cpevtFromTemplate(evtIdOnTheTemplateMap,x,y,direction=undefined);
+ * $gameMap.cpevtFromIdTemplate(id,evtIdOnTheTemplateMap,x,y,direction=undefined);
  * 
  * 回傳值皆為玩家所在的地圖上的新增事件的 id 。
  * 
@@ -11,11 +12,21 @@
  * 沒讀到就不能使用 $gameMap.cpevtFromTemplate ， playtest 時會在遊戲一開始讀取沒讀到時，以 alert 來提醒沒讀到。
  * 例如填 data/Map001.json 
  * 
+ * template map ids: 指定一堆地圖 id 當模板，預設沒東西。
+ * 在 $gameMap.cpevtFromIdTemplate 中第一欄填地圖 id 。
+ * 在遊戲一開始讀取沒讀到時，以 alert 來提醒沒讀到。
+ * 在遊戲中使用沒設定為模板的地圖時，以 alert 來提醒沒讀到。
+ * 
  * 
  * @param TemplateMapPath
  * @type text
  * @text template map path
  * @desc map path for using as the template map
+ * 
+ * @param TemplateMapIds
+ * @type number[]
+ * @text template map ids
+ * @desc map ids for using as a template map
  * 
  * 
  * This plugin can be renamed as you want.
@@ -25,6 +36,7 @@
 const pluginName=getPluginNameViaSrc(document.currentScript.getAttribute('src'))||"agold404_CopyEvent";
 const params=PluginManager.parameters(pluginName)||{};
 params._templateMapPath=params.TemplateMapPath||"";
+params._templateMapIdPaths=[...new Set(JSON.parse(params.TemplateMapIds||"[]").map(x=>x|0))].sort(cmpFunc_num).map(x=>["data/Map"+(""+x).padStart(3,'0')+".json",x]);
 
 t=[
 undefined,
@@ -98,13 +110,22 @@ new cfc(Game_Map.prototype).add('setup',function f(mapid){
 	return newid;
 },undefined,true,true).
 addBase('cpevtFromTemplate',function f(evtid,x,y,d){
-	if(!DataManager._templateMapData) return f.tbl[3]&&alert(f.tbl[2].noTemplate)
+	if(!DataManager._templateMapData) return f.tbl[3]&&alert(f.tbl[2].noTemplate);
 	const evtd=DataManager._templateMapData.events[evtid];
 	const idx=$dataMap.events.push(evtd)-1;
 	arguments[0]=idx;
 	const rtv=this.cpevt.apply(this,arguments);
 	arguments[0]=evtid;
 	return rtv;
+},t).
+addBase('cpevtFromIdTemplate',function f(mapid,evtid,x,y,d){
+	const mapd=DataManager._templateMapIdsData[mapid];
+	if(!mapd) return f.tbl[3]&&alert(f.tbl[2].noTemplate+": "+mapid);
+	const evtd=mapd.events[evtid];
+	const idx=$dataMap.events.push(evtd)-1;
+	const args=[...arguments].slice(1);
+	args[0]=idx;
+	return this.cpevt.apply(this,args);
 },t).
 getP;
 
@@ -117,25 +138,44 @@ new cfc(Game_Event.prototype).add('update',function f(){
 
 new cfc(Scene_Boot.prototype).
 add('start',function f(){
-	this.cpevt_loadTemplateMap();
+	this.cpevt_loadTemplateMaps();
 	return f.ori.apply(this,arguments);
 }).
-addBase('cpevt_loadTemplateMap',function f(){
+addBase('cpevt_loadTemplateMaps',function f(){
 	if(f.tbl[1]._templateMapPath) ImageManager.otherFiles_addLoad(f.tbl[1]._templateMapPath);
+	for(let arr=f.tbl[1]._templateMapIdPaths,xs=arr.length,x=0;x!==xs;++x) ImageManager.otherFiles_addLoad(arr[x][0]);
 },t).
-addBase('cpevt_parseLoadedTemplateMap',function f(){
-	if(!f.tbl[1]._templateMapPath) return;
+addBase('cpevt_parseLoadedTemplateMaps',function f(){
+if(f.tbl[1]._templateMapPath){
 	const raw=ImageManager.otherFiles_getData(f.tbl[1]._templateMapPath);
 	const isTest=f.tbl[3];
-	if(raw===undefined) return isTest&&alert(f.tbl[2].notFound);
+if(raw===undefined) isTest&&alert(f.tbl[2].notFound+": "+f.tbl[1]._templateMapPath);
+else{
 	try{
 		DataManager._templateMapData=JSON.parse(raw);
 	}catch(e){
-		isTest&&alert(f.tbl[2].notJson);
+		isTest&&alert(f.tbl[2].notJson+": "+f.tbl[1]._templateMapPath);
 	}
+}
+} // ._templateMapPath
+
+// ._templateMapIdPaths BEG
+	DataManager._templateMapIdsData={};
+	for(let arr=f.tbl[1]._templateMapIdPaths,xs=arr.length,x=0;x!==xs;++x){
+		const raw=ImageManager.otherFiles_getData(arr[x][0]);
+if(raw===undefined) isTest&&alert(f.tbl[2].notFound+": "+arr[x][0]);
+else{
+		try{
+			DataManager._templateMapIdsData[arr[x][1]]=JSON.parse(raw);
+		}catch(e){
+			isTest&&alert(f.tbl[2].notJson+": "+arr[x][0]);
+		}
+}
+	}
+// ._templateMapIdPaths END
 },t).
 add('terminate',function f(){
-	this.cpevt_parseLoadedTemplateMap();
+	this.cpevt_parseLoadedTemplateMaps();
 	return f.ori.apply(this,arguments);
 }).
 getP;
