773223e6e579394e45e45e2725a385391bdf64c6
agold404 <10041465+aaaaagold@users.noreply.github.com>
2025-03-01 00:01:57 +0800

    update PasteCommonEvent: remove codes from another plugin

--- plugins/agold404_PasteCommonEvent.js
+++ plugins/agold404_PasteCommonEvent.js
@@ -143,58 +143,5 @@ add('onLoad_map_pasteCommonEvents',function f(obj,name,src,msg){
 },t).
 getP;
 
-new cfc(Scene_Boot.prototype).
-add('modEffect1',function f(dataobj,i,arr){
-	this.customItemCostCond.apply(this,arguments);
-	return f.ori.apply(this,arguments);
-}).
-addBase('customItemCostCond',function f(dataobj,i,arr){
-	const meta=dataobj&&dataobj.meta; if(!meta||!dataobj.note) return;
-	meta.customItemUseCond=this.customItemCostCond_parseCond(dataobj);
-	meta.customItemUseCost=this.customItemCostCond_parseCost(dataobj);
-}).
-addBase('customItemCostCond_getScope',function f(dataobj,scopeBegEnd){
-	let rtv='"use strict"; ';
-	let end=0;
-	for(;;){
-		let beg=dataobj.note.indexOf(scopeBegEnd[0],end);
-		if(beg<0) break;
-		end=dataobj.note.indexOf(scopeBegEnd[1],beg);
-		if(end<0) end=dataobj.note.length;
-		rtv+=dataobj.note.slice(beg+scopeBegEnd[0].length,end);
-		rtv+='\n';
-	}
-	return end?rtv:undefined;
-},t).
-addBase('customItemCostCond_parseCond',function f(dataobj){
-	return this.customItemCostCond_getScope(dataobj,f.tbl[2]);
-},t).
-addBase('customItemCostCond_parseCost',function f(dataobj){
-	return this.customItemCostCond_getScope(dataobj,f.tbl[3]);
-},t).
-getP;
-
-new cfc(Game_BattlerBase.prototype).
-add('canUse',function f(item){
-	if(!this.canUse_customEvalItemUseCond(item)) return false;
-	return f.ori.apply(this,arguments);
-}).
-addBase('canUse_customEvalItemUseCond',function f(item){
-	if(!item||!item.meta||item.meta.customItemUseCond==null) return true; // bypass this condition
-	return f.tbl[4](this,item);
-},t).
-getP;
-
-new cfc(Game_Battler.prototype).
-add('useItem',function f(item){
-	this.useItem_customEvalItemUseCost(item);
-	return f.ori.apply(this,arguments);
-}).
-addBase('useItem_customEvalItemUseCost',function f(item){
-	if(!item||!item.meta||item.meta.customItemUseCost==null) return;
-	return f.tbl[5](this,item);
-},t).
-getP;
-
 
 })();
