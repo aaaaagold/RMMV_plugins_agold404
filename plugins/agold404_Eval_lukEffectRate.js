8745eff93dd3ba41000f33f7f6b5e335f5961aa0
agold404 <10041465+aaaaagold@users.noreply.github.com>
2025-07-21 20:23:35 +0800

    add Eval_lukEffectRate

---
+++ plugins/agold404_Eval_lukEffectRate.js
@@ -0,0 +1,41 @@
+﻿"use strict";
+/*:
+ * @plugindesc custom lukEffectRate formula
+ * @author agold404
+ * @help .
+ * 
+ * 
+ * @param LuckEffectRateEval
+ * @type note
+ * @text luck effect rate formula
+ * @desc this text will be passed to eval()
+ * @default "Math.max(1.0 + (this.subject().luk - target.luk) * 0.001, 0.0)"
+ * 
+ * 
+ * This plugin can be renamed as you want.
+ */
+
+(()=>{ let k,r,t;
+const pluginName=getPluginNameViaSrc(document.currentScript.getAttribute('src'))||"agold404_Eval_lukEffectRate";
+const params=PluginManager.parameters(pluginName)||{};
+params._luckEffectRateEval=JSON.parse(params.LuckEffectRateEval||"\"1\"");
+
+
+t=[
+undefined,
+params,
+window.isTest(),
+];
+
+
+new cfc(Game_Action.prototype).
+addBase('lukEffectRate',function f(target){
+	const _s=f.tbl[1]._luckEffectRateEval;
+	{ let f,params,t; {
+		return eval(_s);
+	} }
+},t).
+getP;
+
+
+})();
