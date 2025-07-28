35909db273cf9445170a3140bfbc27e9cc235515
agold404 <10041465+aaaaagold@users.noreply.github.com>
2025-07-28 08:35:57 +0800

    update Api_duplicatedArmors: add getSrcClonedToDstsList

--- plugins/agold404_Api_duplicatedArmors.js
+++ plugins/agold404_Api_duplicatedArmors.js
@@ -32,12 +32,29 @@ window.isTest(),
 ];
 
 
+new cfc(DataManager).
+add('duplicatedDataobj_getSrc',function f(dataobj){
+	const obj=$dataArmors[dataobj&&dataobj[f.tbl[4]]];
+	return obj&&f.ori.apply(this,arguments);
+},t).
+getP;
+
 new cfc(Game_System.prototype).
 addBase('_duplicatedArmors_getCont',function f(){
 	let cont=this._duplicatedArmors_createdRecords; if(!cont) cont=this._duplicatedArmors_createdRecords=[];
 	let m=cont._map_dstArmorId_to_info; if(!m){
 		m=cont._map_dstArmorId_to_info=new Map();
 		for(let x=cont.length;x--;) m.set(cont[x].dstArmorId,cont[x]);
+	}
+	m=cont._map_src_to_dsts; if(!m){
+		m=cont._map_src_to_dsts=new Map();
+		for(let x=cont.length;x--;){
+			const src=$dataArmors[cont[x].srcArmorId];
+			const dst=$dataArmors[cont[x].dstArmorId];
+			if(!src||!dst) continue;
+			if(!m.has(src)) m.set(src,[]);
+			m.get(src).push(dst);
+		}
 	}
 	return cont;
 },t).
@@ -48,6 +65,7 @@ addBase('_duplicatedArmors_clearCont',function f(){
 	const cont=this._duplicatedArmors_getCont();
 	cont.length=0;
 	cont._map_dstArmorId_to_info.clear();
+	cont._map_src_to_dsts.clear();
 	return this;
 },t).
 addBase('_duplicatedArmors_addCreatedRecords',function f(info){
@@ -58,10 +76,26 @@ addBase('_duplicatedArmors_addCreatedRecords',function f(info){
 	else if(f.tbl[2]){ throw new Error(f.tbl[3]+' '+info.dstArmorId); }
 	m.set(info.dstArmorId,idx);
 	cont[idx]=info;
+	{
+		const src=$dataArmors[info.srcArmorId];
+		const dst=$dataArmors[info.dstArmorId];
+		if(src&&dst){
+			const m=cont._map_src_to_dsts;
+			if(!m.has(src)) m.set(src,[]);
+			m.get(src).push(dst);
+		}
+	}
 },t).
 addBase('_duplicatedArmors_getCreatedRecords',function f(dstArmorId){
 	return this._duplicatedArmors_getCont()._map_dstArmorId_to_info.get(dstArmorId);
-},t).
+}).
+addBase('_duplicatedArmors_getSrcClonedToDstsList',function f(srcDataobj){
+	return this._duplicatedArmors_getCont()._map_src_to_dsts.get(srcDataobj);
+}).
+addBase('duplicatedArmors_getSrcClonedToDstsList',function f(srcDataobj){
+	const rtv=this._duplicatedArmors_getSrcClonedToDstsList.apply(this,arguments);
+	return rtv?rtv.slice():[];
+}).
 addBase('duplicatedArmors_createNew',function f(srcArmorId,variationsInfo,newId){
 	// return newly created dataobj id
 	// if variationsInfo is not true-like, return srcArmorId
@@ -104,6 +138,18 @@ add('onAfterLoad_main',function f(){
 	this.duplicatedArmors_onAfterLoad();
 	return f.ori.apply(this,arguments);
 }).
+getP;
+
+
+// glue
+new cfc(Game_System.prototype).
+add('duplicatedDataobj_getSrcClonedToDstsList',function f(srcDataobj){
+	const rtv=f.ori.apply(this,arguments);
+	const res=this._duplicatedArmors_getSrcClonedToDstsList.apply(this,arguments);
+	if(!rtv||!rtv.length) return res&&res.slice();
+	if(res) rtv.concat_inplace(res);
+	return rtv;
+},t).
 getP;
 
 
