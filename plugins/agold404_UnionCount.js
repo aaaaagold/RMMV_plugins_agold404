62cd3f5e7b58f177ad4e4292d3277152f5d45a02
agold404 <10041465+aaaaagold@users.noreply.github.com>
2025-08-01 08:45:20 +0800

    update UnionCount: fix bug

--- plugins/agold404_UnionCount.js
+++ plugins/agold404_UnionCount.js
@@ -27,16 +27,19 @@ kw,
 }
 
 
-new cfc(Scene_Boot.prototype).add('modItem1',function f(dataobj,i,arr){
+new cfc(Scene_Boot.prototype).
+add('modItem1',function f(dataobj,i,arr){
 	this.modItem1_unionCnt.apply(this,arguments);
 	return f.ori.apply(this,arguments);
-}).addBase('modItem1_unionCnt',function f(dataobj,i,arr){
+}).
+addBase('modItem1_unionCnt',function f(dataobj,i,arr){
 	const meta=dataobj&&dataobj.meta; if(!meta) return;
 	if(f.tbl[1] in meta) dataobj[f.tbl[0]]=JSON.parse(meta[f.tbl[1]]);
 },t);
 
-new cfc(Game_Party.prototype).add('unionCnt',function f(dataobj){
-	if(!(dataobj && (f.tbl[0] in dataobj))) return f.ori.apply(this,arguments);
+new cfc(Game_Party.prototype).
+add('unionCnt',function f(dataobj){
+	if(!(dataobj && (f.tbl[0] in dataobj))) return (f.ori||this.numItems).apply(this,arguments);
 	let cnt=0;
 	for(let x=0,arr=dataobj[f.tbl[0]],cont;x!==arr.length;++x){
 		const info=arr[x];
@@ -44,9 +47,11 @@ new cfc(Game_Party.prototype).add('unionCnt',function f(dataobj){
 		if(cont) cnt+=this.numItems(cont[info[1]]);
 	}
 	return cnt;
-},t).addBase('hasMaxItems',function f(dataobj){
+},t).
+addBase('hasMaxItems',function f(dataobj){
 	return this.unionCnt(dataobj)>=(this.maxItems(dataobj)|0);
-});
+}).
+getP;
 
 
 })();
