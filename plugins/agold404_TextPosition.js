c42a422b3548b89f8a02147a7ee828d9b2f2034b
agold404 <10041465+aaaaagold@users.noreply.github.com>
2025-09-20 21:33:35 +0800

    update TextPosition: esc func return value

--- plugins/agold404_TextPosition.js
+++ plugins/agold404_TextPosition.js
@@ -26,35 +26,34 @@
 const pluginName=getPluginNameViaSrc(document.currentScript.getAttribute('src'))||"agold404_TextPosition";
 const params=PluginManager.parameters(pluginName);
 
-new cfc(Window_Base.prototype).add('processEscapeCharacter',function f(code,textState){
+
+new cfc(Window_Base.prototype).
+add('processEscapeCharacter',function f(code,textState){
 	const m=code.match(f.tbl[0]);
-	if(m) return this.processEscapeCharacter_textPosition(code,textState,m);
+	if(m){
+		textState._textPosition_match=m;
+		return this.processEscapeCharacter_textPosition.apply(this,arguments);
+	}
 	return f.ori.apply(this,arguments);
 },[
 /^TXT(LEFT|CENTER|RIGHT|(A|D)POS)$/, // TXTLEFT TXTCENTER TXTRIGHT TXTAPOS
-]).addBase('processEscapeCharacter_textPosition',function f(code,textState,m){
+]).
+addBase('processEscapeCharacter_textPosition',function f(code,textState){
+	const m=textState._textPosition_match; if(m==null) return;
+	delete textState._textPosition_match;
 	// parse new
 	if(textState.text[textState.index]!==":") return console.warn(code,f.tbl[1][0]);
 	const strt=textState.index+1;
 	const strPos=getCStyleStringStartAndEndFromString(textState.text,strt);
 	if(strPos.start!==strt) return console.warn(code,f.tbl[1][0]);
-	//const tailPos=strPos.end-1;
-	//if(tailPos>=this.textPosition_tailPos_getLastPos()) throw new Error(f.tbl[1][1]);
-	//this.textPosition_tailPos_push(tailPos);
-	//textState.index=strPos.start+1;
 	const txt=JSON.parse(textState.text.slice(strPos.start,strPos.end));
 	const func=f.tbl[0][code];
-	if(func) func.call(this,this.duplicateTextState(textState,{
+	const res=func&&func.call(this,this.duplicateTextState(textState,{
 		text:txt,
 		index:0,
 	}),textState);
 	textState.index=strPos.end;
-	return this.processSubtext(txt,textState);
-	const oriTxt=textState.text;
-	textState.index=0;
-	this.drawTextEx(txt,undefined,undefined,undefined,undefined,textState);
-	textState.text=oriTxt;
-	textState.index=strPos.end;
+	return res||this.processSubtext(txt,textState,({prefix:"\\"+code+":",suffix:"",arrange:JSON.stringify,}));
 },[
 {
 TXTLEFT:function(tmpState,textState){
@@ -80,51 +79,22 @@ TXTAPOS:function(tmpState,textState){
 	const pos=EVAL.call(this,tmpState.text); if(!pos) return;
 	if(!isNaN(pos[0]-=0)) textState.x=pos[0];
 	if(!isNaN(pos[1]-=0)) textState.y=pos[1];
-	textState.index+=tmpState.text.length+1;
+	const arr=[]; for(let x=2;x--;) arr[x]=isNaN(pos[x])?"z":pos[x];
+	return !textState.isMeasureOnly&&"\\TXTAPOS:"+JSON.stringify(JSON.stringify(arr));
 },
 TXTDPOS:function(tmpState,textState){
 	const pos=EVAL.call(this,tmpState.text); if(!pos) return;
 	if(!isNaN(pos[0]-=0)) textState.x+=pos[0];
 	if(!isNaN(pos[1]-=0)) textState.y+=pos[1];
-	textState.index+=tmpState.text.length+1;
+	const arr=[]; for(let x=2;x--;) arr[x]=isNaN(pos[x])?"z":pos[x];
+	return !textState.isMeasureOnly&&"\\TXTDPOS:"+JSON.stringify(JSON.stringify(arr));
 },
 }, // 0: functionalities
 [
 " should be followed by an colon(:), then a double quote (\"), and ends with a double quote (\")", // 1-0: warn 
 "error: TextPosition: not support for nested TextPosition", // 1-1: err
 ], // 1: err msg
-]).add('processCharacter',function f(textState){
-	this.processCharacter_textPosition_consumeTailPos(textState);
-	return textState.index<textState.text.length&&f.ori.apply(this,arguments);
-}).addBase('processCharacter_textPosition_consumeTailPos',function f(textState){
-	return; // deprecated
-	while(this.textPosition_tailPos_getLastPos()===textState.index){
-		++textState.index;
-		this.textPosition_tailPos_pop();
-	}
-}).addBase('textPosition_tailPos_getCont',function f(){
-	let rtv=this._textPosition_tailPos; if(!rtv) (rtv=this._textPosition_tailPos=[])._widths={};
-	return rtv;
-}).addBase('textPosition_tailPos_getLastPos',function f(){
-	return this.textPosition_tailPos_getCont().back;
-}).addBase('textPosition_tailPos_getLastWidth',function f(){
-	const cont=this.textPosition_tailPos_getCont();
-	return cont._widths[cont.back];
-}).addBase('textPosition_tailPos_push',function f(idx,width){
-	const cont=this.textPosition_tailPos_getCont();
-	if(idx in cont._widths) throw new Error(f.tbl[0]);
-	idx-=0; if(isNaN(idx)) throw new Error(f.tbl[1]);
-	cont._widths[idx]=width;
-	cont.push(idx);
-	return this;
-},[
-"error: TextPosition: repeated index", // 0: 
-"error: TextPosition: index should be a Number", // 1: 
-]).addBase('textPosition_tailPos_pop',function f(){
-	const cont=this.textPosition_tailPos_getCont();
-	const rtv=cont.pop();
-	delete cont._widths[rtv];
-	return rtv;
-});
+]).
+getP;
 
 })();
