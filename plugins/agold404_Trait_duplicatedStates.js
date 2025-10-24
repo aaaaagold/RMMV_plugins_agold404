6ddb33eb5d70e6020b5a856fcd67bd8b6c07d67a
agold404 <10041465+aaaaagold@users.noreply.github.com>
2025-10-24 22:25:31 +0800

    deprecated: Trait_duplicatedStates

---
+++ plugins/agold404_Trait_duplicatedStates.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+/*
+ * @plugindesc deprecated. use agold404_Trait_statesStackTimes
+ * 
+ * @param GlobalChanges
+ * @type note
+ * @text global change stacked times
+ * @desc input a valid json
+ * @default "0"
+ * 
+ * @help deprecated. use agold404_Trait_statesStackTimes
+ * 
+ * 
+ * This plugin can be renamed as you want.
+ */
+(()=>{
+if(PluginManager._parameters.agold404_Trait_statesStackTimes) return;
+alert(' plugin name agold404_Trait_duplicatedStates has been changed.\n use agold404_Trait_statesStackTimes instead.\n you can turn off this plugin.');
+})();
+
