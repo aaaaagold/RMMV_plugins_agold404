72a10238e2b077ed97b099f812c39256a17b9d63
agold404 <10041465+aaaaagold@users.noreply.github.com>
2026-04-04 15:19:07 +0800

    update Ui_missions: TODO: basket (using Depository)

--- plugins/agold404_Ui_missions.js
+++ plugins/agold404_Ui_missions.js
@@ -87,8 +87,12 @@ undefined, // 3: rsv
 (typeof("")), // 4-1: string type
 ], // 4: separater types
 undefined, // 5: new Set(t[4])
-[], // 6: createAll_parseData - default list
+[], // 6: createAll_parseData - default filters
 {isCalledFromMissionsScene:true,}, // 7: opt to hint it is called from the Scene
+['mission','sep',], // 8: mission list symbols
+[4,4,], // 9: scroll unit
+[4,0.875,true,], // 10: $gameSystem.seEcho_opt_set
+8, // 11: play delay when no seEcho
 ];
 t[5]=new Set(t[4]);
 
@@ -239,17 +243,17 @@ getP;
 new cfc(Game_Temp.prototype).
 addBase('missions_current_show',function f(opt,separator0,matchFunc1){
 	// recommend using Function.apply
-	const cont=this._missions_current_getCont();
-	const arr=this._missions_showList_list=[];
+	const cont=$gameSystem._missions_current_getCont();
+	const arr=this._missions_showList_filters=[];
 	for(let x=1,xs=arguments.length;x<xs;x+=2){
 		let sep=arguments[x];
 		const func=arguments[x+1];
-		if(f.tbl[5].has(typeof(sep))){
+		if(sep&&f.tbl[5].has(typeof(sep))){
 			if(f.tbl[4][0]===typeof(sep)) sep=sep();
 			arr.push(sep);
 		}
 		if(f.tbl[4][0]===typeof(func)){
-			arr.concat_inplace(cont.filter(func));
+			arr.push(func);
 		}
 	}
 	
@@ -264,12 +268,106 @@ const a=class Window_MissionsList extends Window_Command{
 };
 window[a.name]=a;
 new cfc(a.prototype).
-addWithBaseIfNotOwn('initialize',function f(x,y,allList){
+addWithBaseIfNotOwn('initialize',function f(x,y,itemList,opt){
 	const rtv=f.ori.apply(this,arguments);
-	this.initSel(allList);
+	this._hiddenZones={};
+	this._initData_itemList=itemList;
+	this._initData_opt=opt;
+	this.resetItems();
 	this._drawTextExStartOffsetXTimer=0;
 	this.refresh();
 	return rtv;
+}).
+addBase('resetItems',function f(){
+	this.clearCommandList();
+	const itemList=this._initData_itemList;
+	const opt=this._initData_opt;
+	for(let x=0,xs=itemList&&itemList.length,currZone=-1;x<xs;++x){
+		if(f.tbl[4][1]===typeof itemList[x]){
+			// separator
+			currZone=x;
+			this.addCommand(
+				itemList[x],
+				f.tbl[8][1],
+				undefined,
+				currZone,
+			);
+		}else{
+			if(currZone in this._hiddenZones) continue;
+			this.addCommand(
+				$gameSystem.missions_current_getTitle(opt,itemList[x].instanceId),
+				f.tbl[8][0],
+				undefined,
+				itemList[x].instanceId,
+			);
+		}
+	}
+},t).
+addBase('refresh',function f(){
+	this.createContents();
+	Window_Selectable.prototype.refresh.call(this);
+}).
+addBase('refresh_drawDescription',function f(idx){
+	if(idx===undefined) idx=this.index();
+	const wndD=this._descriptionWindow;
+	if(!wndD) return; // init not done
+	wndD.clear_recreateContentsIfOriginallySmaller();
+	if(this.commandSymbol(idx)!==f.tbl[8][0]) return;
+	const opt=undefined;
+	wndD.drawTextEx(
+		$gameSystem.missions_current_getDescription(opt,this.commandExt(idx)),
+		-wndD._scrollX,-wndD._scrollY,
+	);
+	wndD.lastScrollX=wndD._scrollX;
+	wndD.lastScrollY=wndD._scrollY;
+},t).
+addBase('onNewSelect',function f(){
+	this.onNewSelect_drawDescription();
+}).
+addBase('onNewSelect_drawDescription',function f(){
+	const wndD=this._descriptionWindow;
+	if(!wndD) return; // init not done
+	wndD._scrollX=0;
+	wndD._scrollY=0;
+	this.refresh_drawDescription();
+}).
+addBase('toggleZone',function f(idx){
+	if(idx===undefined) idx=this.index();
+	if(this.commandSymbol(idx)!==f.tbl[8][1]) return;
+	const zoneId=this.commandExt(idx);
+	if(zoneId in this._hiddenZones) delete this._hiddenZones[zoneId];
+	else this._hiddenZones[zoneId]=true;
+	this.resetItems();
+	this.refresh();
+},t).
+getP;
+}
+
+{
+const a=class Window_MissionsDescription extends Window_Base{
+};
+window[a.name]=a;
+new cfc(a.prototype).
+addWithBaseIfNotOwn('initialize',function f(x,y,w,h){
+	const rtv=f.ori.apply(this,arguments);
+	this._scrollX=0;
+	this._scrollY=0;
+	this._lastScrollX=0;
+	this._lastScrollY=0;
+	return rtv;
+}).
+addWithBaseIfNotOwn('activate',function f(){
+	const rtv=f.ori.apply(this,arguments);
+	this.contents && this.setCursorRect(0,0,this.contents.width,this.contents.height);
+	return rtv;
+}).
+addWithBaseIfNotOwn('deactivate',function f(){
+	const rtv=f.ori.apply(this,arguments);
+        this.setCursorRect(0,0,0,0);
+	return rtv;
+}).
+addWithBaseIfNotOwn('refresh',function f(){
+	return f.ori.apply(this,arguments);
 }).
 getP;
 }
@@ -285,7 +383,7 @@ addWithBaseIfNotOwn('initialize',function f(){
 	return rtv;
 }).
 addBase('init',function f(){
-	this._state='itemList'; // 'amounts'
+	this._state='itemList'; // cursor position // others: 'detail'
 },t).
 addWithBaseIfNotOwn('create',function f(){
 	const rtv=f.ori.apply(this,arguments);
@@ -305,20 +403,12 @@ addBase('createAll',function f(){
 addBase('createAll_parseData',function f(){
 	// get data from $gameTemp._missions_showList_*
 	this._missionsList_opt=$gameTemp._missions_showList_opt;
-	const arr=this._missionsList_list=$gameTemp._missions_showList_list||f.tbl[6];
-	return;
-	this._missionsList_isSeparator=[];
-	for(let x=0,xs=arr.length;x<xs;++x){
-		this._missionsList_isSeparator[x]=f.tbl[4][1]===typeof arr[x];
-		continue;
-		if(f.tbl[4][1]===typeof arr[x]){
-			this._missionsList_isSeparator[x]=true;
-			this._missionsList_isCompleted[x]=false;
-			this._missionsList_title[x]=arr[x];
-		}else{
-			this._missionsList_isCompleted[x]=$gameSystem.missions_current_chkOk(opt,arr[x].instanceId);
-			this._missionsList_title[x]=$gameSystem.missions_current_getTitle(opt,arr[x].instanceId);
-		}
+	const filters=this._missionsList_filters=$gameTemp._missions_showList_filters||f.tbl[6];
+	const arr=this._missionsList_list=[];
+	const cont=$gameSystem._missions_current_getCont();
+	for(let x=0,xs=filters.length;x<xs;++x){
+		if(f.tbl[4][0]===typeof filters[x]) arr.concat_inplace(cont.filter(filters[x]));
+		else arr.push(filters[x]);
 	}
 },t).
 addBase('createAll_root',function f(){
@@ -326,16 +416,27 @@ addBase('createAll_root',function f(){
 }).
 addBase('createWindow_itemListWindow',function f(){
 	const sp=this._itemListWindow=new Window_MissionsList(0,0,this._missionsList_list,this._missionsList_opt);
+	sp._scene=this;
 	this.getRoot().addChild(sp);
 }).
 addBase('createWindow_itemListWindow_okHandler',function f(){
-	// bind `this` to scene
+	// bind `this` to the scene
+	this._itemListWindow.activate();
 }).
-addBase('createWindow_desciptionWindow',function f(){
-	const sp=this._desciptionWindow=new Window_Base();
+addBase('createWindow_itemListWindow_onclick_separator',function f(){
+	// bind `this` to the scene
+	this._itemListWindow.toggleZone();
+	this._itemListWindow.activate();
+}).
+addBase('createWindow_itemListWindow_onclick_mission',function f(){
+	// bind `this` to the scene
+	this.changeUiState_focusOnDescriptionWnd();
+}).
+addBase('createWindow_descriptionWindow',function f(){
+	const sp=this._descriptionWindow=new Window_MissionsDescription();
 	this.getRoot().addChild(sp);
 }).
-addBase('createWindow_desciptionWindow_refreshContents',function f(){
+addBase('createWindow_descriptionWindow_refreshContents',function f(){
 	;
 }).
 addBase('createAll_finalTune',function f(){
@@ -347,18 +448,20 @@ addBase('createAll_finalTune',function f(){
 	this._descriptionWindow.deactivate();
 	this._itemListWindow.setHandler('cancel',this.popScene.bind(this));
 	this._itemListWindow.setHandler('ok',this.createWindow_itemListWindow_okHandler.bind(this));
+	this._itemListWindow.setHandler('sep',this.createWindow_itemListWindow_onclick_separator.bind(this));
+	this._itemListWindow.setHandler('mission',this.createWindow_itemListWindow_onclick_mission.bind(this));
 	// re-adjust loc
 	//   this._missionsList_opt
 	const itemListWindowPos={
 		x:0,
 		y:0,
-		width:Graphics.width>>1,
+		width:Graphics.width>>2,
 		height:Graphics.height,
 	};
 	const descriptionWindowPos={
 		x:itemListWindowPos.x+itemListWindowPos.width,
 		y:0,
-		width:Graphics.width>>1,
+		width:Graphics.width-itemListWindowPos.width,
 		height:Graphics.height,
 	};
 	this._itemListWindow.position.set(itemListWindowPos.x,itemListWindowPos.y);
@@ -371,10 +474,99 @@ addBase('createAll_finalTune',function f(){
 	this._descriptionWindow.createContents();
 	// display
 	this._itemListWindow.refresh();
-	this._descriptionWindow.refresh();
+	this._itemListWindow.refresh_drawDescription();
+	//this._descriptionWindow.refresh(); // Window_Base has no such method
 	this._itemListWindow.reselect();
 	this._itemListWindow.activate();
 }).
+addWithBaseIfNotOwn('changeUiState_focusOnItemWnd',function f(){
+	this._descriptionWindow.deactivate();
+	this._itemListWindow.activate();
+	this._state='itemList';
+}).
+addWithBaseIfNotOwn('changeUiState_focusOnDescriptionWnd',function f(){
+	this._itemListWindow.deactivate();
+	this._descriptionWindow.activate();
+	this._state='description';
+}).
+addWithBaseIfNotOwn('update',function f(){
+	this.update_handleDescriptionScroll.apply(this,arguments);
+	return f.ori.apply(this,arguments);
+}).
+addWithBaseIfNotOwn('update_handleDescriptionScroll',function f(){
+	if(!this._descriptionWindow.active) return;
+	const wnd=this._descriptionWindow;
+	this._update_handleDescriptionScroll_handleInputs.apply(this,arguments);
+	let shouldRedraw=false;
+	{
+		// scroll
+		if(wnd._scrollX!==wnd._lastScrollX||wnd._scrollY!==wnd._lastScrollY){
+			wnd._lastScrollX=wnd._scrollX;
+			wnd._lastScrollY=wnd._scrollY;
+			shouldRedraw=true;
+			this.update_handleDescriptionScroll_playScrollSe(true);
+		}else{
+			this.update_handleDescriptionScroll_playScrollSe(false);
+		}
+		// update per-frame effects
+		{
+			
+		}
+	}
+	if(shouldRedraw){
+		this._itemListWindow.refresh_drawDescription();
+	}
+}).
+addWithBaseIfNotOwn('_update_handleDescriptionScroll_handleInputs',function f(){
+	const wnd=this._descriptionWindow;
+	const shiftPressed=Input.isPressed('shift');
+	const ctrlPressed=Input.isPressed('control');
+	let dLR=(Input.isPressed('right')-Input.isPressed('left'))*f.tbl[9][0];
+	let dUD=(Input.isPressed('down')-Input.isPressed('up'))*f.tbl[9][1];
+	dLR+=TouchInput.wheelX;
+	dUD+=TouchInput.wheelY;
+	dUD+=wnd.contentsHeight()*(Input.isTriggered('pagedown')-Input.isTriggered('pageup'));
+	dLR<<=shiftPressed;
+	dUD<<=shiftPressed;
+	if(TouchInput.isPressed()){
+		let dd=this._update_handleDescriptionScroll_handleInputs_draggingData;
+		if(!dd) dd=this._update_handleDescriptionScroll_handleInputs_draggingData={x:TouchInput.x,y:TouchInput.y,};
+		dLR+=dd.x-TouchInput.x;
+		dUD+=dd.y-TouchInput.y;
+		dd.x=TouchInput.x;
+		dd.y=TouchInput.y;
+	}else this._update_handleDescriptionScroll_handleInputs_draggingData=undefined;
+	wnd._scrollX=Math.max(0,wnd._scrollX+dLR);
+	wnd._scrollY=Math.max(0,wnd._scrollY+dUD);
+	if(Input.isPressed('home')){
+		wnd._scrollY=0;
+		if(ctrlPressed) wnd._scrollX=0;
+	}
+	if(TouchInput.isCancelled()||Input.isPressed('cancel')){
+		Input.update();
+		TouchInput.update();
+		SoundManager.playCancel();
+		this.changeUiState_focusOnItemWnd();
+	}
+},t).
+addWithBaseIfNotOwn('update_handleDescriptionScroll_playScrollSe',function f(shouldPlay){
+	const lastTimePlayed=this._handleDescriptionScroll_playScrollSe_lastTimePlayed;
+	if(!shouldPlay) this._handleDescriptionScroll_playScrollSe_lastTimePlayed=undefined;
+	const hasSeEcho=$gameSystem&&$gameSystem.seEcho_opt_get;
+	const canPlay=lastTimePlayed==null||(!hasSeEcho&&lastTimePlayed+f.tbl[11]<Graphics.getSceneFrameCnt());
+	const seEchoBak=canPlay&&hasSeEcho&&$gameSystem.seEcho_opt_get();
+	if(hasSeEcho){
+		$gameSystem.seEcho_opt_set({delayFrame:f.tbl[10][0],nextVolRate:f.tbl[10][1],affectStaticSe:f.tbl[10][2],});
+	}
+	if(shouldPlay&&canPlay){
+		this._handleDescriptionScroll_playScrollSe_lastTimePlayed=Graphics.getSceneFrameCnt();
+		this._itemListWindow.playCursorSe();
+	}
+	if(hasSeEcho){
+		if(seEchoBak) $gameSystem.seEcho_opt_set(seEchoBack);
+		else $gameSystem.seEcho_opt_clear();
+	}
+},t).
 getP;
 }
 
@@ -396,7 +588,7 @@ missions_template_add(undefined,{
 }).
 missions_template_add(undefined,{
 	"templateId":"--plugin-examples-2",
-	"titleFunc":()=>"Example-2",
+	"titleFunc":info=>"Example-2 "+info.otherData.timeToComplete,
 	"descriptionFunc":info=>{
 		let s="this is ";
 		s+=$gameSystem.missions_current_getTitle(undefined,info.instanceId);
@@ -404,6 +596,8 @@ missions_template_add(undefined,{
 		s+='\n';
 		const n=Math.max(Math.ceil((info.otherData.timeToComplete-Graphics.getSceneFrameCnt())/60.0),0);
 		s+=" This mission can be completed after "+n+" seconds.";
+		s+='\n';
+		s+="otherData.timeToComplete="+info.otherData.timeToComplete;
 		return s;
 	},
 	"onNewInstance":{
