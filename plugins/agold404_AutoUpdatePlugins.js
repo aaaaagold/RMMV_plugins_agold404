ca8996c9c734e352516d28f20c7140dce44795b2
agold404 <10041465+aaaaagold@users.noreply.github.com>
2026-05-31 13:43:18 +0800

    update AutoUpdatePlugins: version

--- plugins/agold404_AutoUpdatePlugins.js
+++ plugins/agold404_AutoUpdatePlugins.js
@@ -22,7 +22,7 @@
  */
 
 (()=>{ let k,r,t;
-const ver='2025-05-30 0',verJudgeKey='_agold404_AutoUpdatePlugins_isVerJudging';
+const ver='2025-05-31 0',verJudgeKey='_agold404_AutoUpdatePlugins_isVerJudging';
 { const key=verJudgeKey; if(window[key]){
 	return ver;
 } }
